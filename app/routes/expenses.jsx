180c7cbd3f853fd712fca1c7e8870d0a8f72dd76
Joshhortt <joshhortt@yahoo.com>
2023-01-04 16:35:06 +0000

    moved/renamed (analysis): component from expenses to routes

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,14 +1,33 @@
 // /expenses => shared layout
 
 import { Outlet } from "@remix-run/react";
+import ExpensesList from "~/components/expenses/ExpensesList";
 import expensesStyles from "~/styles/expenses.css";
+
+const TESTING_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 101.99,
+    date: new Date().toISOString(),
+  },
+
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 102.99,
+    date: new Date().toISOString(),
+  },
+];
 
 export default function ExpensesLayout() {
   return (
-    <main>
-      <p>Shared element!</p>
+    <>
       <Outlet />
-    </main>
+      <main>
+        <ExpensesList expenses={TESTING_EXPENSES} />
+      </main>
+    </>
   );
 }
 
