5a434f26c82a3f79e1674ea5817acd247f247784
Joshhortt <joshhortt@yahoo.com>
2023-01-04 16:06:55 +0000

    add (analysis): testing   component route with data

--- app/routes/expenses/analysis.jsx
+++ app/routes/expenses/analysis.jsx
@@ -1,5 +1,28 @@
 // /expenses/analysis
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import Chart from "~/components/expenses/Chart";
+
+const TESTING_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 101.99,
+    date: new Date().toISOString(),
+  },
+
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 102.99,
+    date: new Date().toISOString(),
+  },
+];
 
 export default function ExpensesAnalysisPage() {
-  return <h1>Expenses Analysis Page</h1>;
+  return (
+    <main>
+      <Chart expenses={TESTING_EXPENSES} />
+      <ExpenseStatistics expenses={TESTING_EXPENSES} />
+    </main>
+  );
 }
