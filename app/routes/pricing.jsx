8437ef33e3239e57da3b104d550630b7297e2b40
Joshhortt <joshhortt@yahoo.com>
2023-01-04 19:02:37 +0000

    add (pricing): page with marketing content.

--- app/routes/pricing.jsx
+++ app/routes/pricing.jsx
@@ -1,5 +1,51 @@
 // /pricing
 
+// /pricing
+
+import { FaTrophy, FaHandshake } from "react-icons/fa";
+
+import PricingPlan from "~/components/marketing/PricingPlan";
+
+const PRICING_PLANS = [
+  {
+    id: "p1",
+    title: "Basico", // Basic
+    price: "Grátis", // Free forever
+    perks: ["1 User", "Up to 100 expenses/year", "Basic analytics"],
+    icon: FaHandshake,
+  },
+  {
+    id: "p2",
+    title: "Pro",
+    price: "$9.99/mês", //$9.99/month
+    perks: [
+      "Utilizadores ilimitados",
+      "Despesas/ano ilimitadas",
+      "Análise detalhada",
+    ], // "Unlimited Users", "Unlimited expenses/year", "Detailed analytics"
+    icon: FaTrophy,
+  },
+];
+
 export default function PricingPage() {
-  return <h1>Pricing Page</h1>;
+  return (
+    <main id="pricing">
+      {/* Great Product, Simple Pricing */}
+      <h2>Grande Produto, Preços Acessíveis</h2>
+      <ol id="pricing-plans">
+        {PRICING_PLANS.map((plan) => (
+          <li key={plan.id} className="plan">
+            <PricingPlan
+              title={plan.title}
+              price={plan.price}
+              perks={plan.perks}
+              icon={plan.icon}
+            />
+          </li>
+        ))}
+      </ol>
+    </main>
+  );
 }
+
+export function meta() {}
