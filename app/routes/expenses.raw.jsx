8d19d74d02f87f5f614553b0cab501ba3ede24b7
Joshhortt <joshhortt@yahoo.com>
2023-01-05 19:33:19 +0000

    test: Resource and Splat routes

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,5 +1,21 @@
 // /expenses/raw
 
-export default function ExpensesRawPage() {
-  return <h1>Expenses Raw Page</h1>;
+const TESTING_EXPENSES = [
+  {
+    id: "e1",
+    title: "Primeira Despesa",
+    amount: 101.99,
+    date: new Date().toISOString(),
+  },
+
+  {
+    id: "e2",
+    title: "Segunda Despesa",
+    amount: 102.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export function loader() {
+  return TESTING_EXPENSES;
 }
