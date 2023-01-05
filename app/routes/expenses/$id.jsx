7a57f43d366bded07fd0c7fbf6fb49e6494048e5
Joshhortt <joshhortt@yahoo.com>
2023-01-05 16:30:08 +0000

    feat: Function closeHandler to close Modal navigating away

--- app/routes/expenses/$id.jsx
+++ app/routes/expenses/$id.jsx
@@ -1,11 +1,19 @@
 // /expenses/<some-id> => expenses/expense-1 OR expenses/e-1,
 
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function UpdateExpensesPage() {
+  const navigate = useNavigate();
+  function closeHandler() {
+    //navigate programmatically
+    navigate("..");
+    // navigate("/expenses");
+  }
+
   return (
-    <Modal>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
--- app/routes/expenses/add.jsx
+++ app/routes/expenses/add.jsx
@@ -1,11 +1,19 @@
 // /expenses/add
 
+import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
 
 export default function AddExpensesPage() {
+  const navigate = useNavigate();
+  function closeHandler() {
+    //navigate programmatically
+    navigate("..");
+    // navigate("/expenses");
+  }
+
   return (
-    <Modal>
+    <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
