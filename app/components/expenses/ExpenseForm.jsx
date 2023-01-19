cd115306a0271eebb16fdca6d3121297af287050
Joshhortt <joshhortt@yahoo.com>
2023-01-19 13:20:01 +0000

    feat: Add visual submission feedback

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,11 +1,35 @@
-import { Link, useActionData } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  useActionData,
+  useTransition as useNavigation,
+} from "@remix-run/react";
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-01-10
   const validationErrors = useActionData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== "idle";
+
+  // const submit = useSubmit();
+
+  // function submitHandler(event) {
+  //   event.preventDefault();
+  //   // perform your own validation
+  //   // ...
+  //   submit(event.target, {
+  //     // action: '/expenses/add',
+  //     method: "post",
+  //   });
+  // }
 
   return (
-    <form method="post" className="form" id="expense-form">
+    <Form
+      method="post"
+      className="form"
+      id="expense-form"
+      // onSubmit={submitHandler}
+    >
       <p>
         {/* Expense Title */}
         <label htmlFor="title">Nome da Despesa</label>
@@ -39,13 +63,15 @@ function ExpenseForm() {
       )}
       <div className="form-actions">
         {/* Save Expense */}
-        <button>Guardar</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "a guardar..." : "Guardar"}
+        </button>
         {/* Cancel */}
         <Link to="..">Cancelar</Link>
         {/* This line of code below also cancels - goes to the parent route */}
         {/* <Link to="/expenses">Cancelar</Link> */}
       </div>
-    </form>
+    </Form>
   );
 }
 
