9c7abbb03fe6a36af13d8a760842e67cd484227f
Joshhortt <joshhortt@yahoo.com>
2023-01-19 14:19:44 +0000

    feat: Fetch data via Loader

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -14,3 +14,15 @@ export async function addExpense(expenseData) {
     throw new Error("Failed to add expense.");
   }
 }
+
+export async function getExpenses() {
+  try {
+    const expenses = await prisma.expense.findMany({
+      orderBy: { date: "desc" },
+    });
+    return expenses;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -1,26 +1,29 @@
 // /expenses => shared layout
 
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import { FaPlus, FaDownload } from "react-icons/fa";
 import ExpensesList from "~/components/expenses/ExpensesList";
+import { getExpenses } from "~/data/expenses.server";
 
-const TESTING_EXPENSES = [
-  {
-    id: "e1",
-    title: "Primeira Despesa",
-    amount: 101.99,
-    date: new Date().toISOString(),
-  },
+// const TESTING_EXPENSES = [
+//   {
+//     id: "e1",
+//     title: "Primeira Despesa",
+//     amount: 101.99,
+//     date: new Date().toISOString(),
+//   },
 
-  {
-    id: "e2",
-    title: "Segunda Despesa",
-    amount: 102.99,
-    date: new Date().toISOString(),
-  },
-];
+//   {
+//     id: "e2",
+//     title: "Segunda Despesa",
+//     amount: 102.99,
+//     date: new Date().toISOString(),
+//   },
+// ];
 
 export default function ExpensesLayout() {
+  const expenses = useLoaderData();
+
   return (
     <>
       <Outlet />
@@ -33,11 +36,15 @@ export default function ExpensesLayout() {
           <a href="/expenses/raw">
             <FaDownload />
             {/* Load raw data */}
-            <span>Carrega dados brutos</span>
+            <span>Carregar Dados </span>
           </a>
         </section>
-        <ExpensesList expenses={TESTING_EXPENSES} />
+        <ExpensesList expenses={expenses} />
       </main>
     </>
   );
+}
+
+export function loader() {
+  return getExpenses();
 }
