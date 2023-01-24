995151dffa459e13bf0e03313a306bb835ffb795
Joshhortt <joshhortt@yahoo.com>
2023-01-24 17:56:16 +0000

    feat: function login -compare 2 error cases

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -1,4 +1,4 @@
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 
 import { prisma } from "./database.server";
 
@@ -14,4 +14,26 @@ export async function signup({ email, password }) {
   const passwordHash = await hash(password, 12);
 
   await prisma.user.create({ data: { email: email, password: passwordHash } });
+}
+
+export async function login({ email, password }) {
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+
+  if (!existingUser) {
+    const error = new Error(
+      "Não foi possível iniciar sessão, por favor verifique as credenciais fornecidas."
+    );
+    error.status = 401;
+    throw error;
+  }
+
+  const passwordCorrect = await compare(password, existingUser.password);
+
+  if (!passwordCorrect) {
+    const error = new Error(
+      "Não foi possível iniciar sessão, por favor verifique as credenciais fornecidas."
+    );
+    error.status = 401;
+    throw error;
+  }
 }
