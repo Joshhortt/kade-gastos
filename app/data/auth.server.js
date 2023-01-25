0cc49f5832a75eea0d6f8cda0626f09237084639
Joshhortt <joshhortt@yahoo.com>
2023-01-25 10:50:56 +0000

    feat: Add require user session route protection

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -50,6 +50,14 @@ export async function destroyUserSession(request) {
   });
 }
 
+export async function requireUserSession(request) {
+  const userId = await getUserFromSession(request);
+
+  if (!userId) {
+    throw redirect("/auth?mode=login");
+  }
+}
+
 export async function signup({ email, password }) {
   const existingUser = await prisma.user.findFirst({ where: { email } });
 
--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -3,6 +3,7 @@ import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import { FaPlus, FaDownload } from "react-icons/fa";
 
 import ExpensesList from "~/components/expenses/ExpensesList";
+import { requireUserSession } from "~/data/auth.server";
 import { getExpenses } from "~/data/expenses.server";
 
 export default function ExpensesLayout() {
@@ -38,7 +39,8 @@ export default function ExpensesLayout() {
   );
 }
 
-export async function loader() {
+export async function loader({ request }) {
+  await requireUserSession(request);
   const expenses = await getExpenses();
   return expenses;
 
